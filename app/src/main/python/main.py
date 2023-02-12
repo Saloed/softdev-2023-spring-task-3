386af25c534dab14b81b4f2201b616bf0d947d82
Plezha <plehanovtemka@gmail.com>
2023-02-13 00:37:19 +0300

    2nd commit. Who cares?

--- app/src/main/python/main.py
+++ app/src/main/python/main.py
@@ -1,8 +1,22 @@
-
 def main(toExec, toEval):
     try:
-        print(f'{toExec = }\n{toEval = }')
-        exec(toExec)
-        return eval(toEval)
+        try:
+            exec(toExec, globals())
+        except Exception as Error:
+            return f"Error while trying to exec(): {Error}"
+
+        try:
+            if toEval == "":
+               result = ""
+            else:
+               result = eval(toEval)
+               if type(result) is str: result = "\"" + str(result) + "\""
+        except Exception as Error:
+            return f"Error while trying to eval(): {Error}"
+
+
+
+        return str(result)
     except Exception as Error:
         return Error
+
