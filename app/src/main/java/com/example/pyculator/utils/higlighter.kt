3c1ae70935f115882c0ea431d29b86115c263609
Plezha <plehanovtemka@gmail.com>
2023-05-07 17:24:47 +0300

    Highlighting by ANTLR4

--- app/src/main/java/com/example/pyculator/utils/higlighter.kt
+++ app/src/main/java/com/example/pyculator/utils/higlighter.kt
@@ -1,5 +1,8 @@
 package com.example.pyculator.utils
 
+import Python3BaseListener
+import Python3Lexer
+import Python3Parser
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
@@ -7,12 +10,8 @@ import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
+import org.antlr.v4.runtime.*
 
-private val keywordsRegex = Regex(
-    """\s(False|None|True|__peg_parser__|and|as|assert|async|await|break|class|
-    |continue|def|del|elif|else|except|finally|for|from|global|if|import|in|is|lambda|nonlocal|not|
-    |or|pass|raise|return|try|while|with|yield)(:|\s)""".trimMargin()
-)
 private val functionsRegex = Regex(
     """\s(abs|aiter|all|any|anext|ascii|bin|bool|breakpoint|bytearray|bytes|
     |callable|chr|classmethod|compile|complex|delattr|dict|dir|divmod|enumerate|eval|exec|filter|
@@ -21,61 +20,135 @@ private val functionsRegex = Regex(
     |print|property|range|repr|reversed|round|set|setattr|slice|sorted|staticmethod|str|
     |sum|super|tuple|type|vars|zip|__import__)\s""".trimMargin()
 )
-private val operatorsRegex = Regex(
-    """=|
-        |==|!=|<|<=|>|>=|
-        |\+|-|\*|/|//|%|\*\*|
-        |\+=|-=|\*=|/=|%=|
-        |^|\||&|~|>>|<<""".trimMargin()
-)
-private val braces = mapOf(
-    '(' to ')', '{' to '}', '[' to ']',
-)
-private val bracesRegex = Regex("""[(){}\[\]]""")
-private val strRegex = Regex("""([rfbu]?'[^'\\\n]*(\\.[^'\\\n]*)*')|([rfbu]?"[^"\\\n]*(\\.[^"\\\n]*)*")""")
-
-private val bigStrRegex = Regex("""[rfbu]?(\"\"\"(.|\n)*\"\"\")|
-    |(\'\'\'(.|\n)*\'\'\')""".trimMargin())
-
-private val numericRegex = Regex("""(\b[+-]?[0-9_]+[lL]?\b)|
-    |(\b[+-]?0[xX][0-9A-Fa-f_]+[lL]?\b)|
-    |(\b[+-]?[0-9_]+(?:\.[0-9_]+)?(?:[eE][+-]?[0-9_]+)?\b)""".trimMargin()
-) // Decimal Hex Float
-
 private val commentRegex = Regex("""#.*""")
 
+private val styles = mutableSetOf<AnnotatedString.Range<SpanStyle>>()
+
+private fun addStyle(spanStyle: SpanStyle, start: Int, end: Int) {
+    styles.add(
+        AnnotatedString.Range(
+            spanStyle,
+            start,
+            end,
+        )
+    )
+}
+private fun addStyle(color: Color, start: Int, end: Int) {
+    addStyle(SpanStyle(color = color), start, end)
+}
+
+private fun addStyle(color: Color, token: Token) {
+    addStyle(color, token.startIndex, token.stopIndex + 1)
+}
+private fun addStyle(color: Color, range: IntRange) {
+    addStyle(color, range.first, range.last + 1)
+}
+
+
+private fun getColor(
+    token: Token,
+): Color? {
+    var color: Color? = null
+    val type = token.type
+    if (type == Python3Lexer.STRING) { // Strings
+        color = Color.Green
+    }
+    if (type in Python3Lexer.DEF..Python3Lexer.AWAIT) { // Keywords
+        color = Color.Magenta
+    }
+    if (type in setOf(
+            Python3Lexer.OPEN_BRACE,
+            Python3Lexer.OPEN_BRACK,
+            Python3Lexer.OPEN_PAREN,
+            Python3Lexer.CLOSE_BRACE,
+            Python3Lexer.CLOSE_BRACK,
+            Python3Lexer.CLOSE_PAREN,
+        )
+    ) {
+        color = Color.Yellow
+    }
+    if (type in setOf(
+            Python3Lexer.INTEGER,
+            Python3Lexer.BIN_INTEGER,
+            Python3Lexer.DECIMAL_INTEGER,
+            Python3Lexer.HEX_INTEGER,
+            Python3Lexer.OCT_INTEGER,
+            Python3Lexer.NUMBER,
+            Python3Lexer.FLOAT_NUMBER,
+            Python3Lexer.IMAG_NUMBER,
+        )
+    ) {
+        color = Color.Yellow
+    }
+    if (type in setOf(
+            Python3Lexer.STAR,
+            Python3Lexer.POWER,
+            Python3Lexer.ASSIGN,
+            Python3Lexer.OR_OP,
+            Python3Lexer.XOR,
+            Python3Lexer.AND_OP,
+            Python3Lexer.LEFT_SHIFT,
+            Python3Lexer.RIGHT_SHIFT,
+            Python3Lexer.ADD,
+            Python3Lexer.MINUS,
+            Python3Lexer.DIV,
+            Python3Lexer.MOD,
+            Python3Lexer.IDIV,
+            Python3Lexer.LESS_THAN,
+            Python3Lexer.GREATER_THAN,
+            Python3Lexer.EQUALS,
+            Python3Lexer.LT_EQ,
+            Python3Lexer.GT_EQ,
+            Python3Lexer.NOT_EQ_1,
+            Python3Lexer.NOT_EQ_2,
+            Python3Lexer.AT,
+            Python3Lexer.ADD_ASSIGN,
+            Python3Lexer.AND_ASSIGN,
+            Python3Lexer.SUB_ASSIGN,
+            Python3Lexer.POWER_ASSIGN,
+            Python3Lexer.AT_ASSIGN,
+            Python3Lexer.DIV_ASSIGN,
+            Python3Lexer.IDIV_ASSIGN,
+            Python3Lexer.LEFT_SHIFT_ASSIGN,
+            Python3Lexer.DIV_ASSIGN,
+            Python3Lexer.MOD_ASSIGN,
+            Python3Lexer.MULT_ASSIGN,
+            Python3Lexer.RIGHT_SHIFT_ASSIGN,
+            Python3Lexer.OR_ASSIGN,
+            Python3Lexer.XOR_ASSIGN,
+        )
+    ) {
+        color = Color.Yellow
+    }
+    return color
+}
+
+class HighlighterListener() : Python3BaseListener() {
+    override fun enterEveryRule(ctx: ParserRuleContext?) {
+        super.enterEveryRule(ctx)
+        val token = ctx?.start
+        if (token != null) {
+            val color = getColor(token)
+            if (color != null) addStyle(color, token)
+        }
+    }
+}
+
+
+
 fun highlight(text: String, errorString: String): AnnotatedString {
+    val parser = Python3Parser( CommonTokenStream( Python3Lexer( ANTLRInputStream(text) ) ) )
+    val listener = HighlighterListener()
+    parser.addParseListener(listener)
+    parser.file_input()
+
 
     val errorStringSplit = errorString.split(" ")
     val errorLine = if (errorString == "OK") -1 else errorStringSplit[errorStringSplit.size - 2].toInt()
     val errorChar = if (errorString == "OK") -1 else errorStringSplit.last().toInt()
 
-    val styles = mutableListOf<AnnotatedString.Range<SpanStyle>>()
-    // Extremely inefficient
-    for (i in keywordsRegex.findAll(text)) {
-        styles.add(AnnotatedString.Range(SpanStyle(color = Color.Magenta), i.range.first, i.range.last+1))
-    }
-    for (i in functionsRegex.findAll(text)) {
-        styles.add(AnnotatedString.Range(SpanStyle(color = Color.Red), i.range.first, i.range.last+1))
-    }
-    for (i in operatorsRegex.findAll(text)) {
-        styles.add(AnnotatedString.Range(SpanStyle(color = Color.Yellow), i.range.first, i.range.last+1))
-    }
-    for (i in bracesRegex.findAll(text)) {
-        styles.add(AnnotatedString.Range(SpanStyle(color = Color.Yellow), i.range.first, i.range.last+1))
-    }
-    for (i in strRegex.findAll(text)) {
-        styles.add(AnnotatedString.Range(SpanStyle(color = Color.Green), i.range.first, i.range.last+1))
-    }
-    for (i in bigStrRegex.findAll(text)) {
-        styles.add(AnnotatedString.Range(SpanStyle(color = Color.Green), i.range.first, i.range.last+1))
-    }
-    for (i in numericRegex.findAll(text)) {
-        styles.add(AnnotatedString.Range(SpanStyle(color = Color.Yellow), i.range.first, i.range.last+1))
-    }
-    for (i in commentRegex.findAll(text)) {
-        styles.add(AnnotatedString.Range(SpanStyle(color = Color.Gray), i.range.first, i.range.last+1))
-    }
+    for (i in functionsRegex.findAll(text)) addStyle(Color.Red, i.range)
+    for (i in commentRegex.findAll(text)) addStyle(Color.Gray, i.range)
 
     if (errorString != "OK") {
         var line = 1
@@ -89,48 +162,17 @@ fun highlight(text: String, errorString: String): AnnotatedString {
                 charInLine = 1
             }
             if (line == errorLine && charInLine == errorChar) {
-                styles.add(
-                    AnnotatedString.Range(
-                        SpanStyle(
-                            textDecoration = TextDecoration.Underline,
-                            color = Color.Red
-                        ), totalChar, totalChar + 1
-                    )
+                addStyle(
+                    SpanStyle(textDecoration = TextDecoration.Underline, color = Color.Red),
+                    totalChar,
+                    totalChar + 1
                 )
             }
         }
     }
-
-
-
-
-    // Braces checker
-    val s = mutableListOf<Pair<Char, Int>>()
-    var skip = false
-    for ((i, c) in text.withIndex()) {
-        // Commented braces don't count
-        if (c == '#') skip = true
-        if (c == '\n') skip = false
-        if (skip) continue
-
-        if (braces.containsKey(c)) {
-            s.add((c to i))
-        } else if (s.isNotEmpty() && braces[s.last().first] == c) {
-            s.removeLast()
-        } else if (braces.containsValue(c)) {
-            // Closing brace has no pair
-            styles.add(AnnotatedString.Range(SpanStyle(color = Color.Red), i, i+1))
-        }
-    }
-    for (i in s) {
-        // Opening brace has no pair
-        styles.add(AnnotatedString.Range(SpanStyle(color = Color.Red), i.second, i.second+1))
-    }
-
-
     return AnnotatedString(
         text = text,
-        spanStyles = styles
+        spanStyles = styles.toList()
     )
 }
 
