59617949c197073677350e323e6fb52f5c2d7a00
Plezha <plehanovtemka@gmail.com>
2023-05-09 00:29:36 +0300

    Heh e)

--- app/src/main/java/com/example/pyculator/utils/higlighter.kt
+++ app/src/main/java/com/example/pyculator/utils/higlighter.kt
@@ -10,14 +10,6 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
 import org.antlr.v4.runtime.*
 
-private val functionsRegex = Regex(
-    """\s(abs|aiter|all|any|anext|ascii|bin|bool|breakpoint|bytearray|bytes|
-    |callable|chr|classmethod|compile|complex|delattr|dict|dir|divmod|enumerate|eval|exec|filter|
-    |float|format|frozenset|getattr|globals|hasattr|hash|help|hex|id|input|int|isinstance|
-    |issubclass|iter|len|list|locals|map|max|memoryview|min|next|object|oct|open|ord|pow|
-    |print|property|range|repr|reversed|round|set|setattr|slice|sorted|staticmethod|str|
-    |sum|super|tuple|type|vars|zip|__import__)\s""".trimMargin()
-)
 private val commentRegex = Regex("""#.*""")
 
 private val styles = mutableSetOf<AnnotatedString.Range<SpanStyle>>()
@@ -154,7 +146,6 @@ fun highlight(text: String, errorString: String): AnnotatedString {
     val errorLine = if (errorString == "OK") -1 else errorStringSplit[errorStringSplit.size - 2].toInt()
     val errorChar = if (errorString == "OK") -1 else errorStringSplit.last().toInt()
 
-    for (i in functionsRegex.findAll(text)) addStyle(Color.Red, i.range)
     for (i in commentRegex.findAll(text)) addStyle(Color.Gray, i.range)
 
     if (errorString != "OK") {
