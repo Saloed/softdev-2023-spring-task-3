f669a474f3cfac45c668dd93fce6ffbc9b71c09f
Plezha <plehanovtemka@gmail.com>
2023-05-09 00:19:16 +0300

    Better antlr usage

--- app/src/main/java/com/example/pyculator/utils/higlighter.kt
+++ app/src/main/java/com/example/pyculator/utils/higlighter.kt
@@ -1,8 +1,6 @@
 package com.example.pyculator.utils
 
-import Python3BaseListener
 import Python3Lexer
-import Python3Parser
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
@@ -47,102 +45,110 @@ private fun addStyle(color: Color, range: IntRange) {
 
 private fun getColor(
     token: Token,
-): Color? {
-    var color: Color? = null
-    val type = token.type
-    if (type == Python3Lexer.STRING) { // Strings
-        color = Color.Green
-    }
-    if (type in Python3Lexer.DEF..Python3Lexer.AWAIT) { // Keywords
-        color = Color.Magenta
-    }
-    if (type in setOf(
-            Python3Lexer.OPEN_BRACE,
-            Python3Lexer.OPEN_BRACK,
-            Python3Lexer.OPEN_PAREN,
-            Python3Lexer.CLOSE_BRACE,
-            Python3Lexer.CLOSE_BRACK,
-            Python3Lexer.CLOSE_PAREN,
-        )
-    ) {
-        color = Color.Yellow
-    }
-    if (type in setOf(
-            Python3Lexer.INTEGER,
-            Python3Lexer.BIN_INTEGER,
-            Python3Lexer.DECIMAL_INTEGER,
-            Python3Lexer.HEX_INTEGER,
-            Python3Lexer.OCT_INTEGER,
-            Python3Lexer.NUMBER,
-            Python3Lexer.FLOAT_NUMBER,
-            Python3Lexer.IMAG_NUMBER,
-        )
-    ) {
-        color = Color.Yellow
-    }
-    if (type in setOf(
-            Python3Lexer.STAR,
-            Python3Lexer.POWER,
-            Python3Lexer.ASSIGN,
-            Python3Lexer.OR_OP,
-            Python3Lexer.XOR,
-            Python3Lexer.AND_OP,
-            Python3Lexer.LEFT_SHIFT,
-            Python3Lexer.RIGHT_SHIFT,
-            Python3Lexer.ADD,
-            Python3Lexer.MINUS,
-            Python3Lexer.DIV,
-            Python3Lexer.MOD,
-            Python3Lexer.IDIV,
-            Python3Lexer.LESS_THAN,
-            Python3Lexer.GREATER_THAN,
-            Python3Lexer.EQUALS,
-            Python3Lexer.LT_EQ,
-            Python3Lexer.GT_EQ,
-            Python3Lexer.NOT_EQ_1,
-            Python3Lexer.NOT_EQ_2,
-            Python3Lexer.AT,
-            Python3Lexer.ADD_ASSIGN,
-            Python3Lexer.AND_ASSIGN,
-            Python3Lexer.SUB_ASSIGN,
-            Python3Lexer.POWER_ASSIGN,
-            Python3Lexer.AT_ASSIGN,
-            Python3Lexer.DIV_ASSIGN,
-            Python3Lexer.IDIV_ASSIGN,
-            Python3Lexer.LEFT_SHIFT_ASSIGN,
-            Python3Lexer.DIV_ASSIGN,
-            Python3Lexer.MOD_ASSIGN,
-            Python3Lexer.MULT_ASSIGN,
-            Python3Lexer.RIGHT_SHIFT_ASSIGN,
-            Python3Lexer.OR_ASSIGN,
-            Python3Lexer.XOR_ASSIGN,
-        )
-    ) {
-        color = Color.Yellow
-    }
-    return color
-}
+) = when(token.type) {
+        Python3Lexer.STRING -> Color.Green
 
-class HighlighterListener() : Python3BaseListener() {
-    override fun enterEveryRule(ctx: ParserRuleContext?) {
-        super.enterEveryRule(ctx)
-        val token = ctx?.start
-        if (token != null) {
-            val color = getColor(token)
-            if (color != null) addStyle(color, token)
-        }
+        Python3Lexer.DEF,
+        Python3Lexer.RETURN,
+        Python3Lexer.RAISE,
+        Python3Lexer.FROM,
+        Python3Lexer.IMPORT,
+        Python3Lexer.AS,
+        Python3Lexer.GLOBAL,
+        Python3Lexer.NONLOCAL,
+        Python3Lexer.ASSERT,
+        Python3Lexer.IF,
+        Python3Lexer.ELIF,
+        Python3Lexer.ELSE,
+        Python3Lexer.WHILE,
+        Python3Lexer.FOR,
+        Python3Lexer.IN,
+        Python3Lexer.TRY,
+        Python3Lexer.FINALLY,
+        Python3Lexer.WITH,
+        Python3Lexer.EXCEPT,
+        Python3Lexer.LAMBDA,
+        Python3Lexer.OR,
+        Python3Lexer.AND,
+        Python3Lexer.NOT,
+        Python3Lexer.IS,
+        Python3Lexer.NONE,
+        Python3Lexer.TRUE,
+        Python3Lexer.FALSE,
+        Python3Lexer.CLASS,
+        Python3Lexer.YIELD,
+        Python3Lexer.DEL,
+        Python3Lexer.PASS,
+        Python3Lexer.CONTINUE,
+        Python3Lexer.BREAK,
+        Python3Lexer.ASYNC,
+        Python3Lexer.AWAIT, -> Color.Magenta
+
+        Python3Lexer.OPEN_BRACE,
+        Python3Lexer.OPEN_BRACK,
+        Python3Lexer.OPEN_PAREN,
+        Python3Lexer.CLOSE_BRACE,
+        Python3Lexer.CLOSE_BRACK,
+        Python3Lexer.CLOSE_PAREN -> Color.Yellow
+
+        Python3Lexer.INTEGER,
+        Python3Lexer.BIN_INTEGER,
+        Python3Lexer.DECIMAL_INTEGER,
+        Python3Lexer.HEX_INTEGER,
+        Python3Lexer.OCT_INTEGER,
+        Python3Lexer.NUMBER,
+        Python3Lexer.FLOAT_NUMBER,
+        Python3Lexer.IMAG_NUMBER -> Color.Yellow
+
+        Python3Lexer.STAR,
+        Python3Lexer.POWER,
+        Python3Lexer.ASSIGN,
+        Python3Lexer.OR_OP,
+        Python3Lexer.XOR,
+        Python3Lexer.AND_OP,
+        Python3Lexer.LEFT_SHIFT,
+        Python3Lexer.RIGHT_SHIFT,
+        Python3Lexer.ADD,
+        Python3Lexer.MINUS,
+        Python3Lexer.DIV,
+        Python3Lexer.MOD,
+        Python3Lexer.IDIV,
+        Python3Lexer.LESS_THAN,
+        Python3Lexer.GREATER_THAN,
+        Python3Lexer.EQUALS,
+        Python3Lexer.LT_EQ,
+        Python3Lexer.GT_EQ,
+        Python3Lexer.NOT_EQ_1,
+        Python3Lexer.NOT_EQ_2,
+        Python3Lexer.AT,
+        Python3Lexer.ADD_ASSIGN,
+        Python3Lexer.AND_ASSIGN,
+        Python3Lexer.SUB_ASSIGN,
+        Python3Lexer.POWER_ASSIGN,
+        Python3Lexer.AT_ASSIGN,
+        Python3Lexer.DIV_ASSIGN,
+        Python3Lexer.IDIV_ASSIGN,
+        Python3Lexer.LEFT_SHIFT_ASSIGN,
+        Python3Lexer.MOD_ASSIGN,
+        Python3Lexer.MULT_ASSIGN,
+        Python3Lexer.RIGHT_SHIFT_ASSIGN,
+        Python3Lexer.OR_ASSIGN,
+        Python3Lexer.XOR_ASSIGN -> Color.Yellow
+
+        else -> null
     }
-}
-
-
 
 fun highlight(text: String, errorString: String): AnnotatedString {
     styles.clear()
-    val parser = Python3Parser( CommonTokenStream( Python3Lexer( ANTLRInputStream(text) ) ) )
-    val listener = HighlighterListener()
-    parser.addParseListener(listener)
-    parser.file_input()
+    val lexer = Python3Lexer( ANTLRInputStream(text) )
+    var token: Token
+    var color: Color?
 
+    do  {
+        token = lexer.nextToken()
+        color = getColor(token)
+        if (color != null) addStyle(color, token)
+    } while (token.type != -1)
 
     val errorStringSplit = errorString.split(" ")
     val errorLine = if (errorString == "OK") -1 else errorStringSplit[errorStringSplit.size - 2].toInt()
