887ab958beb5cfd35928418214e20facbdfa04ef
Chrisnisch <mihail.gusev.95@yandex.ru>
2023-06-01 14:40:18 +0300

    "harpseal" font working, and some other fixes

--- src/main/java/org/game/view_control/App.java
+++ src/main/java/org/game/view_control/App.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -14,6 +15,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader(getClass().getResource("game_scene.fxml"));
+        Font.loadFont(Objects.requireNonNull(getClass().getResource("Harpseal.ttf")).toExternalForm(), 10);
         Parent root = loader.load();
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
